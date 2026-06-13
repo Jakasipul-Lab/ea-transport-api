954f14520509766e3e4095f9693201ecda6e6f9d
osare nakinson <owangochief@gmail.com>
2026-06-13 20:00:49 +0200

    Change server run command to uvicorn CLI

--- server.py
+++ server.py
@@ -118,4 +118,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     print(f"DEBUG: Starting server on port {port}")
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn server:app --host 0.0.0.0 --port 8000)
