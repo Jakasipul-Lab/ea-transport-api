f422c395c1f7446fb3307807408521ef30d079df
osare nakinson <owangochief@gmail.com>
2026-06-21 23:07:14 +0200

    Change default port from 8000 to 10000

--- server.py
+++ server.py
@@ -252,9 +252,5 @@ def home():
 
 # The block below is the very last thing in the file
 if __name__ == "__main__":
-    import uvicorn
-    import os
-    
-    # This allows the host platform to tell your app which port to use
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run("server:app", host="0.0.0.0", port=port)
