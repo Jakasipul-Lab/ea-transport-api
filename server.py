4d641cabcc58f6bd64f52974673e45e744d240fa
osare nakinson <owangochief@gmail.com>
2026-07-04 23:52:09 +0200

    Remove FastAPI app setup and home route
    Removed FastAPI app initialization and home route.

--- server.py
+++ server.py
@@ -75,13 +75,6 @@ def about():
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/")
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
 # --------------------------------------
 # ✅ LOCAL SEARCH - BLUE CARDS
 # --------------------------------------
