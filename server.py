e44b63f3cfbb4e5b567073c4ed7a9582f14290f6
osare nakinson <owangochief@gmail.com>
2026-07-04 13:33:50 +0200

    Change default port from 8080 to 8085

--- server.py
+++ server.py
@@ -20,5 +20,5 @@ async def admin(request: Request):
 
 if __name__ == "__main__":
     # Render provides the port in the PORT environment variable
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8085))
     uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
