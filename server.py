4f336b0c096a04c1ef8fecff351906756bd446b6
osare nakinson <owangochief@gmail.com>
2026-07-04 13:00:17 +0200

    Update route decorators to specify methods

--- server.py
+++ server.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import uvicorn
-import random
-import string
 
 app = FastAPI(title="OSARE Hub")
 
@@ -18,15 +16,15 @@ async def fix_query_params(request: Request, call_next):
     response = await call_next(request)
     return response
 
-@app.get("/")
+@app.get("/", methods=["GET", "HEAD"])
 async def home(request: Request):
     return templates.TemplateResponse("safari.html", {"request": request})
 
-@app.get("/dashboard")
+@app.get("/dashboard", methods=["GET", "HEAD"])
 async def dashboard(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
 
-@app.get("/admin")
+@app.get("/admin", methods=["GET", "HEAD"])
 async def admin(request: Request):
     return templates.TemplateResponse("admin.html", {"request": request})
 
