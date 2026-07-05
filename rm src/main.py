c98d99998f98294b9fed1d41e8c478acf9c05f1f
osare nakinson <owangochief@gmail.com>
2026-07-05 05:06:48 +0200

    Add FastAPI application in main.py

---
+++ rm src/main.py
@@ -0,0 +1,23 @@
+cat > src/main.py << 'EOF'
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+templates = Jinja2Templates(directory="src/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+@app.get("/safari", response_class=HTMLResponse)
+async def safari(request: Request):
+    return templates.TemplateResponse("safari.html", {"request": request})
+EOF
