649e1dec40986d2a122ae604f43142736911d676
osare nakinson <owangochief@gmail.com>
2026-07-29 20:37:45 +0200

    Update server.py

--- server.py
+++ server.py
@@ -60,6 +60,19 @@ def api_search(category: str = Query("safari"), q: str = ""):
             
     return JSONResponse(content=results)
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+  return "Hello, World!"
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
